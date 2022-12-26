3256a5e78a3d1d435cc692f5186631ffb16eef33
anbo <anbo225@qq.com>
2022-12-26 14:58:39 +0800

    echo "Hello World" to qemu

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
 
-fn main() {
-    // println!("Hello, world!");
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
-/// 这个函数将在 panic 时被调用
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
